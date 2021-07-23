f56b845088de121bfe29de66cf1b8d32b154f712
Denis Arana <darana.desarrollo@gmail.com>
2021-07-23 14:19:09 -0600

    Unique identifier for every new game
    Add a unique identifier when a new game is created

--- Game.java
+++ Game.java
@@ -1,13 +1,43 @@
 package com.deviget.api;
 
+import java.util.UUID;
+
 public class Game {
 
 	//properties for a new game
+	private String game_id;
+	
 	private int win_width;
 	private int win_height;
 	private int num_rows;
 	private int num_columns;
 	private int num_mines;
+	
+	
+
+	public String getGame_id() {
+		return game_id;
+	}
+
+	public int getWin_width() {
+		return win_width;
+	}
+
+	public int getWin_height() {
+		return win_height;
+	}
+
+	public int getNum_rows() {
+		return num_rows;
+	}
+
+	public int getNum_columns() {
+		return num_columns;
+	}
+
+	public int getNum_mines() {
+		return num_mines;
+	}
 
 	//Initialization properties for a new game
 	public void create(int win_width, int win_height, int num_rows, int num_columns, int num_mines) {
@@ -16,7 +46,13 @@ public class Game {
 		this.num_rows = num_rows;
 		this.num_columns = num_columns;
 		this.num_mines = num_mines;
+		this.game_id=generateId();
 
+	}
+	
+	public String generateId(){
+	    UUID uuid = UUID.randomUUID();
+	    return uuid.toString();
 	}
 
 }
